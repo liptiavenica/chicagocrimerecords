e760adbc2163f71115b70eca9239b5693fe289af
Jadequeline <jademp.it@gmail.com>
2019-11-27 02:39:26 +0700

    heatmap

---
+++ assets/js/crime_heatmap.js
@@ -0,0 +1,74 @@
+var map = L.map('map').setView([41.8752130174, -87.6292057552], 10);
+
+
+var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+}).addTo(map);
+
+var heat;
+
+var currentZoom = map.getZoom();
+
+function getRadius(){
+            var radius;
+            if (currentZoom === 10){
+                radius=20
+            }
+            else if (currentZoom === 11) {
+                radius = 22;
+            }
+            else if (currentZoom === 12) {
+                radius = 24;
+            }
+            else if (currentZoom === 13) {
+                radius = 26;
+            }
+            else if (currentZoom === 14) {
+                radius = 28;
+            }
+            else if (currentZoom === 15) {
+                radius = 30;
+            }
+            else if (currentZoom === 16) {
+                radius = 32;
+            }
+            else if (currentZoom === 17) {
+                radius = 34;
+            }
+            else if (currentZoom === 18) {
+                radius = 36;
+            }
+            return radius;
+}
+
+drawMap()
+
+function drawMap (crimeIndex = crimeIdx(crimeWeight = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])) {
+
+    if (heat) map.removeLayer(heat);
+
+    var dataCrime = data.map(function (p,i) { return [p["Latitude"], p["Longitude"],crimeIndex[i]] });
+
+    heat = L.heatLayer(dataCrime, {
+        radius: getRadius(),
+        blur: 20, 
+        maxZoom: 16,
+        minOpacity: 0.7
+    }).addTo(map);
+
+    map.on('zoomend', function(ev) {
+        currentZoom = map.getZoom()
+
+        heat.setOptions({
+            radius: getRadius(),
+            blur: 20,
+            minOpacity: 0.7
+        });
+        heat.redraw();
+    });
+
+    return dataCrime
+        
+}
+
---
+++ assets/js/crime_index.js
@@ -0,0 +1,31 @@
+var crimes_statistic = crimes_statistic
+var crimes_type = ['ARSON', 'ASSAULT', 'BATTERY', 'BURGLARY',
+'CONCEALED CARRY LICENSE VIOLATION', 'CRIM SEXUAL ASSAULT',
+'CRIMINAL DAMAGE', 'CRIMINAL TRESPASS', 'DECEPTIVE PRACTICE',
+'GAMBLING', 'HOMICIDE', 'HUMAN TRAFFICKING',
+'INTERFERENCE WITH PUBLIC OFFICER', 'INTIMIDATION', 'KIDNAPPING',
+'LIQUOR LAW VIOLATION', 'MOTOR VEHICLE THEFT', 'NARCOTICS',
+'NON-CRIMINAL', 'OBSCENITY', 'OFFENSE INVOLVING CHILDREN',
+'OTHER OFFENSE', 'PROSTITUTION', 'PUBLIC PEACE VIOLATION',
+'ROBBERY', 'SEX OFFENSE', 'STALKING', 'THEFT', 'WEAPONS VIOLATION']
+
+function crimesType (index = 0) {
+    return crimes_type[index]
+}
+
+function crimeIdx (crimeWeight = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]){
+    var crimeIdx = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for (crime in crimes_type){
+        var crime_name = crimes_type[crime]
+        var crime_statistic = crimes_statistic[crime_name]
+        var crime_max = crime_statistic['max']
+        var crime_min = crime_statistic['min']
+        // console.log(crime_name, crime_max, crime_min)
+        for (district in crime_statistic['crimes_per_district']){
+            console.log(district)
+            var crime_district = crime_statistic['crimes_per_district'][district]
+            crimeIdx[district-1] += crimeWeight[district-1] * (crime_district - crime_min) / (crime_max-crime_min)
+        }
+    }
+    return crimeIdx 
+}
